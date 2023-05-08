1bd1d2138b1e8410f7a9744e9b917c0363f87daf
dylanjphelps <dphelpsj@gmail.com>
2023-05-07 23:10:08 -0500

    add url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ const config = {
   favicon: "images/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-test-site.com",
+  url: "https://alittlefaun.onrender.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
